fc008db1df2c9367724486243d5a4f1951c8330e
Dennis-89 <straub.dennis1@web.de>
2023-12-06 22:02:42 +0100

    Finished part 1 of day 3

--- day3.py
+++ day3.py
@@ -1,3 +1,4 @@
+import contextlib
 import re
 from pathlib import Path
 
@@ -22,22 +23,36 @@ def parse_input(lines):
     line_to_symbols = {number: [] for number in range(len(lines))}
     for line_number, line in enumerate(lines):
         for match in re.finditer(r"\d+", line):
-            line_to_parts[line_number].append(match.span())
+            line_to_parts[line_number].append(
+                {
+                    "start": int(match.start()),
+                    "end": int(match.end()),
+                    "number": int(match.group(0)),
+                }
+            )
         for match in re.finditer(r"[^\d.\n]", line):
-            line_to_symbols[line_number].append(match.span()[0])
+            line_to_symbols[line_number].append(int(match.start()))
     return line_to_parts, line_to_symbols
 
-# TODO in Process
+
+def looking_for_symbol(line_to_parts, line_to_symbols):
+    found_parts = []
+    for line, positions in line_to_parts.items():
+        for position_details in positions:
+            for x in range(position_details["start"] - 1, position_details["end"] + 1):
+                for delta in [-1, 0, 1]:
+                    with contextlib.suppress(KeyError):
+                        if x in line_to_symbols[line + delta]:
+                            found_parts.append(position_details["number"])
+    return found_parts
+
+
+# TODO Part 2 is missing
 def main():
     engine_parts = list(INPUT.read_text(encoding="UTF-8").splitlines())
+    # engine_parts = EXAMPLE_LINES
     line_to_parts, line_to_symbols = parse_input(engine_parts)
-    found_parts = []
-    print(line_to_parts)
-    print(line_to_symbols)
-    for line, position in line_to_symbols.items():
-        if position - 1 in line_to_parts[line]:
-            found_parts.append(engine_parts[line])
-
+    print(sum(looking_for_symbol(line_to_parts, line_to_symbols)))
 
 
 if __name__ == "__main__":
