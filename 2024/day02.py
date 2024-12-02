1d980d50428d56868c90f9ef9b8aead6ff6dd1f6
Dennis-89 <straub.dennis1@web.de>
2024-12-02 21:26:53 +0100

    Add day02

---
+++ 2024/day02.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+from itertools import pairwise
+from pathlib import Path
+
+
+INPUT_FILE = Path(__file__).parent / "input02.txt"
+PUZZLE_INPUT = """\
+7 6 4 2 1
+1 2 7 8 9
+9 7 6 2 1
+1 3 2 4 5
+8 6 4 4 1
+1 3 6 7 9
+""".splitlines()
+
+
+def is_save(report):
+    return all(1 <= x - y <= 3 for x, y in pairwise(report)) or all(
+        1 <= y - x <= 3 for x, y in pairwise(report)
+    )
+
+
+def get_save_reports(reports):
+    return sum(is_save(report) for report in reports)
+
+
+def main():
+    puzzle_input = PUZZLE_INPUT
+    puzzle_input = INPUT_FILE.read_text(encoding="UTF-8").splitlines()
+    reports = list(map(lambda x: list(map(int, x.split())), puzzle_input))
+    print(get_save_reports(reports))
+
+
+if __name__ == "__main__":
+    main()
